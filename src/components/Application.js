fcf86de3d1402517ed9020abbf69fd1f347595ce
ahmed <achau1019@gmail.com>
2021-09-05 21:10:49 -0400

    Adjust proxy, get days from API using axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import DayList from "./DayList";
 
 import "components/Application.scss";
 import 'components/Appointment';
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -68,8 +51,16 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
 
-  const schedule = appointments.map(appointment => <Appointment key={appointment.id} {...appointment}/>)
+  const schedule = appointments.map(appointment => <Appointment key={appointment.id} {...appointment}/>);
+
+  useEffect(() => {
+    axios.get('/api/days').then(response => {
+      console.log(response.data);
+      setDays([...response.data]);
+    })
+  }, []);
 
   return (
     <main className="layout">
@@ -82,8 +73,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
             day={day}
+            days={days}
             setDay={setDay}
           />
         </nav>
