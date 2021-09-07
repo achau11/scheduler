5b5c3da6eab52d70995df5f506317580b021f794
ahmed <achau1019@gmail.com>
2021-09-07 07:25:25 -0400

    implement custom hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,7 +46,6 @@ export default function Application(props) {
     })
   }, []);
 
-  console.log(state.interviewers);
   return (
     <main className="layout">
       <section className="sidebar">
